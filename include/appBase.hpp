735539ef8fd4f49e1004f138bcc6e37ef397b2b2
Jintao Yu <1007936662@qq.com>
2024-10-08 23:13:46 +0200

    remove submodule bgfx

--- include/appBase.hpp
+++ include/appBase.hpp
@@ -1,6 +1,12 @@
 namespace ips {
 
-class AppBase {};
+class AppBase {
+public:
+  virtual ~AppBase() = 0;
+  virtual void init() = 0;
+  virtual void update() = 0;
+  virtual void shutdown() = 0;
+};
 
 int runApp(AppBase *_app, int _argc, char **_agrv);
 } // namespace ips
